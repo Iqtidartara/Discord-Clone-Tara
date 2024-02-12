09a59723a039b6052facb1d5c5749f3c306c8304
Iqtidartara <135112726+Iqtidartara@users.noreply.github.com>
2024-02-12 18:28:06 +0500

    First Commit

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,16 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+
+  return (
+    <div>
+      <UserButton
+      afterSignOutUrl="/"
+      />
+<ModeToggle />
+
+      
+    </div>
+  );
+}
