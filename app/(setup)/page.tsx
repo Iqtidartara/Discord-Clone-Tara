9125d00634915d78a6dcac319091c105afced95b
Iqtidartara <135112726+Iqtidartara@users.noreply.github.com>
2024-02-13 16:24:15 +0500

    First commit

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,23 @@
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+import { InitialModal } from "@/components/modals/initial-modal";
+
+const SetupPage = async () => {
+    const profile = await initialProfile();
+    const server = await db.server.findFirst({
+        where: {
+            members :{
+                some:{
+                    profileId : profile.id
+                }
+            }
+        }
+    })
+    if(server){
+        return  redirect(`/servers/${server.id}`)
+    }
+    return <InitialModal/>;
+}
+ 
+export default SetupPage;
---
+++ components/modals/initial-modal.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+
+import { Dialog,
+DialogContent,
+DialogDescription,
+DialogFooter,
+DialogHeader,
+DialogTitle
+} from "../ui/dialog"
+
+export const InitialModal =() =>{
+    return(
+        <div>
+            This will be a modal!
+        </div>
+    )
+}
